e73c3328a72454d0b974a2c4bac2160b7addc109
rendazhur <423217875@qq.com>
2020-02-13 00:10:36 +0800

    查询全部产品完成

--- heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
+++ heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
@@ -1,5 +1,7 @@
 package com.itheima.ssm.domain;
 
+import com.itheima.ssm.utils.DateUtils;
+
 import java.util.Date;
 
 /**
@@ -61,6 +63,9 @@ public class Product {
     }
 
     public String getDepartureTimeStr() {
+        if(departureTime != null){
+            departureTimeStr = DateUtils.date2String(departureTime,"yyyy-MM-dd HH:mm:ss");
+        }
         return departureTimeStr;
     }
 
@@ -93,6 +98,12 @@ public class Product {
     }
 
     public String getProductStatusStr() {
+        if(productStatus != null) {
+            if (productStatus == 0)
+                productStatusStr = "关闭";
+            if (productStatus == 1)
+                productStatusStr = "开启";
+        }
         return productStatusStr;
     }
 
